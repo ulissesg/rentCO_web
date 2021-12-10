4029a078f3766cff81519d7808abeff39f99e369
ulissesg <ulissesg2018@gmail.com>
2021-12-09 21:01:45 -0300

    first connection to the api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Menu from './components/Menu';
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Client from './components/Client';
 
 function App() {
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,6 +1,30 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
+
 function Client () {
+    const [client, setClient] = useState();
+
+    useEffect(() => {
+    api
+      .get("/client/list")
+      .then((response) => {
+        //   setClient(response.data);
+            console.log(response.data)
+        })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+//   console.log(client[1]);
+
     return (
+        
+        
         <div>
+            {client}
             Client general page
         </div>
     )
